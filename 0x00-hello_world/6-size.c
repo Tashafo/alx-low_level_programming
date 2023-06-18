6cd185de771bd6ec415d8a8578f008f61e281da5
Tashafo <natashaokonti@yahoo.com>
2023-06-18 14:36:07 -0700

    I hope this is the end for real

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*Return: Always 0 (Success)
-*Return always 0 indicates sucessful program execution 
-*Return value 0 indicates successful program termination  
-*prints sizes of various variable types in C
-*/
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Return always 0 indicates successful program execution
+ * Return value 0 indicates successful program termination
+ * prints sizes of various variable types in C
+ */
 int main(void)
 {
-printf("Size of a char: %zu byte(s)\n", sizeof(char));
-printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %zu byte(s)\n", sizeof(float));
-return (0);
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %zu byte(s)\n", sizeof(float));
+    return (0);
 }
