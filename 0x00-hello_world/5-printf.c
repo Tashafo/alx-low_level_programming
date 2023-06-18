79954ccdce75908aa012a26611fe92db1eb5b64b
Tashafo <natashaokonti@yahoo.com>
2023-06-17 17:48:35 -0700

    part of me

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- * Return always 0 indicates successful program execution
- * Return value 0 indicates successful program termination
- */
+* main - Entry point
+* Return: Always 0 (Success)
+* Return always 0 indicates successful program execution
+* Return value 0 indicates successful program termination
+*/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a work of art\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
 }
